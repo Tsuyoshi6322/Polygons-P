a9f5ffffea5c95d092f5b4358f232309b4be56f1
Tsuyoshi6322 <tsutsu6322@gmail.com>
2025-02-08 00:00:35 +0100

    app.py draft

--- app.py
+++ app.py
@@ -1,3 +1,41 @@
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
+# =============== IMPORT DANYCH =============== 
+def data_file_import():
+    print("Import danych z pliku")
+
+def data_manual_import():
+    print("Dane manualnie wprowadzone")
+
+# =============== ZDEFINIOWANIE FIGURY =============== 
+def polygon_calculate_points():
+    print("Obliczenie ilosci punktow")
+
+def polygon_is_triangle():
+    print("Typ trójkąta")
+
+def polygon_is_quadrilateral():
+    print("Typ czworokąta")
+
+def polygon_define_main():
+    print("Zdefiniowanie figury")
+
+# =============== OBLICZENIE POLA I OBWODU FIGURY =============== 
+def polygon_calculate_area():
+    print("Obliczenie pola figury")
+
+def polygon_calculate_perimeter():
+    print("Obliczenie obwodu figury")
+
+def polygon_calculate_main():
+    print("Wynikowe pole, figura i obwód")
+
+# =============== EXPORT DANYCH =============== 
+def data_file_export():
+    print("Eksport wyniku do pliku")
+
+
+# =============== MAIN =============== 
+def main():
+    print("Skąd dane wprowadzić?")
+
+if __name__ == "__main__":
+    main()
