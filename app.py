ade4b0c76d1d00d9e018c5cd8a45522240d1f34e
Tsuyoshi6322 <tsutsu6322@gmail.com>
2025-02-08 01:13:25 +0100

    data_import works app.py

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
+import os
+
 # =============== PUNKTY =============== 
 class Point:
-    def __inti__(self, x, y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
-# =============== ZARZADZANIE PLIKIEM =============== 
-def data_file_exists():
-    print("Czy plik data.txt istnieje?")
+    def __repr__(self):
+        return f"({self.x},{self.y})"
 
-def data_file_import():
-    print("Import danych z pliku")
+# =============== ZARZADZANIE PLIKIEM =============== 
+def data_file_exists(file_path):
+    #print("Czy plik data.txt istnieje?")
+    if not os.path.exists(file_path):
+        print(f"Błąd: Plik data.txt nie istnieje!")# PH
+
+
+def data_file_import(file_path):
+    #print("Import danych z pliku")
+    labels = ["A", "B", "C", "D"]
+
+    with open(file_path, "r") as file:
+        data = list(map(int, file.read().strip().split(";")))
+
+    return {labels[i]: Point(data[i * 2], data[i * 2 + 1]) for i in range(len(labels))}
+
 
 def data_manual_import():
     print("Dane manualnie wprowadzone")
+
 
 def data_file_export():
     print("Eksport wyniku do pliku")
@@ -21,11 +37,14 @@ def data_file_export():
 def polygon_calculate_points():
     print("Obliczenie ilosci punktow")
 
+
 def polygon_is_triangle():
     print("Typ trójkąta")
 
+
 def polygon_is_quadrilateral():
     print("Typ czworokąta")
+
 
 def polygon_define_main():
     print("Zdefiniowanie figury")
@@ -34,15 +53,25 @@ def polygon_define_main():
 def polygon_calculate_area():
     print("Obliczenie pola figury")
 
+
 def polygon_calculate_perimeter():
     print("Obliczenie obwodu figury")
+
 
 def polygon_calculate_main():
     print("Wynikowe pole, figura i obwód")
 
 # =============== MAIN =============== 
 def main():
-    print("Głowna funkcja")
+    
+    # Import funkcji
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    file_path = os.path.join(script_dir, "data.txt")
+
+    points = data_file_import(file_path)
+
+    print(", ".join(f"{label}{point}" for label, point in points.items()))
 
 if __name__ == "__main__":
     main()
